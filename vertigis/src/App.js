11430a3cac7f0537d375411ac387d94e57e73b00
Konstantin-exe <konstantinmuellner@gmail.com>
2021-05-03 22:16:21 +0200

    Steop 5 done

--- vertigis/src/App.js
+++ vertigis/src/App.js
@@ -1,44 +1,40 @@
 import './App.css';
-import { esri } from './bundle';
+// import { esri } from './bundle';
 
-esri();
-function esriConfig() {
-  esriConfig.apiKey =
-    'AAPKdec769a768d9408d87b28ce841f964fcMd1Z8FCI87tdg0sFVKgDBg_MyKC50dx4lReYyt1Tq2HuOiaJD7WLmFsh3E6zvrRD';
-}
+// esri();
+// function esriConfig() {
+//   esriConfig.apiKey =
+//     'AAPKdec769a768d9408d87b28ce841f964fcMd1Z8FCI87tdg0sFVKgDBg_MyKC50dx4lReYyt1Tq2HuOiaJD7WLmFsh3E6zvrRD';
+// }
 
-function Map() {
-  const map = new Map({
-    basemap: 'arcgis-topographic',
-  });
-}
+// function Map() {
+//   const map = new Map({
+//     basemap: 'arcgis-topographic',
+//   });
+// }
 
-function MapView() {
-  const map = new Map({
-    basemap: 'arcgis-topographic',
-  });
-  const view = new MapView({
-    map: map,
-    center: [-118.805, 34.027], // Longitude, latitude
-    zoom: 13, // Zoom level
-    container: 'viewDiv', // Div element
-  });
-}
+// function MapView() {
+//   const map = new Map({
+//     basemap: 'arcgis-topographic',
+//   });
+//   const view = new MapView({
+//     map: map,
+//     center: [-118.805, 34.027], // Longitude, latitude
+//     zoom: 13, // Zoom level
+//     container: 'viewDiv', // Div element
+//   });
+// }
 
-function renderMap() {
-  esriConfig();
-  Map();
-  MapView();
-}
+// function renderMap() {
+//   esriConfig();
+//   Map();
+//   MapView();
+// }
 
-renderMap();
+// renderMap();
 
 function App() {
-  return (
-    <div className="App">
-      <div id="viewDiv"></div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
