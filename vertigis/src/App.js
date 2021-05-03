dc9bcffd5f1be4d7c5e7fb75b2c3f705ece21e2a
Konstantin-exe <konstantinmuellner@gmail.com>
2021-05-03 17:10:46 +0200

    I tried requiere/import - failed

--- vertigis/src/App.js
+++ vertigis/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { esri } from './bundle';
+
+esri();
+function esriConfig() {
+  esriConfig.apiKey =
+    'AAPKdec769a768d9408d87b28ce841f964fcMd1Z8FCI87tdg0sFVKgDBg_MyKC50dx4lReYyt1Tq2HuOiaJD7WLmFsh3E6zvrRD';
+}
+
+function Map() {
+  const map = new Map({
+    basemap: 'arcgis-topographic',
+  });
+}
+
+function MapView() {
+  const map = new Map({
+    basemap: 'arcgis-topographic',
+  });
+  const view = new MapView({
+    map: map,
+    center: [-118.805, 34.027], // Longitude, latitude
+    zoom: 13, // Zoom level
+    container: 'viewDiv', // Div element
+  });
+}
+
+function renderMap() {
+  esriConfig();
+  Map();
+  MapView();
+}
+
+renderMap();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="viewDiv"></div>
     </div>
   );
 }
---
+++ vertigis/src/bundle.js
@@ -0,0 +1,4 @@
+function esri() {
+  require(['esri/config', 'esri/Map', 'esri/views/MapView']);
+}
+export default esri;
